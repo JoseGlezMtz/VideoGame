4d743e596c0835aecbc9c9c7ae1ea7be6f0a08b0
JoseGlez <A01028517@tec.mx>
2024-05-01 11:50:52 -0600

    Finishing the change card mechanic and modifying the cards prefabs

--- VideogameProject/tablero_forest/Assets/Scripts/CardManager.cs
+++ VideogameProject/tablero_forest/Assets/Scripts/CardManager.cs
@@ -5,10 +5,9 @@ using UnityEngine.UI;
 
 public class CardManager : MonoBehaviour{
     [SerializeField] GameObject CardPrefab;
-    
-
+    [SerializeField] Transform parentPrefab;
     public List<GameObject> Cartas_mano=new List<GameObject>();
-    public List<GameObject> Cartas_jugando=new List<GameObject>();
+    [SerializeField] public List<GameObject> Panels=new List<GameObject>();
     public bool Change_Option=false;
     public GameObject obj1=null;
     public GameObject obj2=null;
@@ -19,61 +18,36 @@ public class CardManager : MonoBehaviour{
         InitGame();
     }
 
-//referencia para crear todas las cartas 
-    public void Card_base(int x,int y,List<GameObject> Lista){
-            GameObject Card=Instantiate(CardPrefab);
-            Card.transform.position = new Vector3(x,y,0);
+    //template of al  the cards in the board 
+    public void Card_base(List<GameObject> Lista,int i){
+            GameObject Card=Instantiate(CardPrefab, Panels[i].transform.position, Quaternion.identity, parentPrefab);
             Card.AddComponent<Atributos_cartas>();
             Card.name="Card"+Lista.Count;
             Lista.Add(Card);
+            Debug.Log("Creating: " + Card.name);
             Button button= Card.GetComponent<Button>();
             button.onClick.AddListener(()=> registerCard(Card));
-            Material material = Card.GetComponent<Renderer>().material;
-            material.color = new Color(0, 1, 0);
+            Card.GetComponent<Image>().color = Color.HSVToRGB((float)i/Panels.Count, 1, 1);
+            
     }
     
-    // Crea las cartas en la banca
+    // creat cards in the board
     IEnumerator Creat_card(){
-        int x=-2;
-        int y=-3;
         int i=0;
-        while (i<3){
-            yield return new WaitForSeconds(1.0f);
-            Card_base(x,y,Cartas_mano);
-            x+=2; 
-            i++;
-        }
-        x=6;
-        y=0;
-        i=0;
-
-        // Genera las cartas iniciale a jugar
-        while (i<2){
-            yield return new WaitForSeconds(1.0f);
-            Card_base(x,y,Cartas_mano);
-            y+=3; 
-            i++;
-        }
-        x=-6;
-        y=-0;
-        i=0;
-
-        //Genera las cartas del enemigo
-        while (i<2){
-            yield return new WaitForSeconds(1.0f);
-            GameObject Card=Instantiate(CardPrefab);
-            Card.transform.position = new Vector3(x,y,0);
-            Card.AddComponent<Atributos_cartas>();
-            Card.name="Card_Enemy"+i;
-            y+=3; 
+        while(i< Panels.Count){
+            Card_base(Cartas_mano,i);
+            yield return new WaitForSeconds(0.1f);
             i++;
         }
         
     }
+
+    //init the game
     void InitGame(){
         StartCoroutine(Creat_card());
     }
 
+    //This function changes the position of two cards
     public void Change_Cards(GameObject obj1, GameObject obj2){
         Vector3 temp=obj1.transform.position;
         obj1.transform.position=obj2.transform.position;
@@ -81,17 +55,36 @@ public class CardManager : MonoBehaviour{
     }
 
     public void registerCard(GameObject objeto_carta){
-        if (Change_Option==true){
-            if (obj2==null){
-                obj2 = objeto_carta;
+        //First we check if the cahnge option is active
+        if (Change_Option){
+            //We check if the card is in the enemy hand
+            if (Cartas_mano.IndexOf(objeto_carta)>4 ){
+            Debug.Log("Enemy card, can't change it");
+            }
+            //if it is a card in the player hand we stablish the first card to change
+            else{ if (obj1==null){
+                obj1 = objeto_carta;
+                Debug.Log("Changing with "+obj1.name);
+                }
+                //If the first card is already stablished we stablished the seconf card and change the cards
+                else{
+                obj2=objeto_carta;
+                Debug.Log("Changing with "+ obj2.name);
                 Change_Cards(obj1,obj2);
-            }
-
-            else{
-                obj1=objeto_carta;
-            }
+                //We reset the variables
+                obj1=null;
+                obj2=null;
+                Change_Option=false;    
+                }
+                }
+        }
+        //If the change option is not active we send error message
+        else {
+            Debug.Log("Invalid option");
         }
     }
+
+    //This function is used to change the state of the change option(active in the change button)
     public void Change_State(){
         Change_Option=true;
         
