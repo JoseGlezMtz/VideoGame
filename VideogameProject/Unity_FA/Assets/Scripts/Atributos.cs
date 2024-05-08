4403ecf924b7beb4a5509aa39a1d9f781348295d
Enrique Martinez <a01783060@tec.mx>
2024-05-07 23:54:29 -0600

    Avance de mecanicas de ataque

---
+++ VideogameProject/Unity_FA/Assets/Scripts/Atributos.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Atributos : MonoBehaviour
+{
+    // Propiedades para los atributos de la carta
+    public int HP { get; set; } // Puntos de vida
+    public int Attack { get; set; } // Ataque
+}
