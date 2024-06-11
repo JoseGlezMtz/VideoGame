e06202ab643b4517dec9ac586392b387d3041c3f
paydelimon22 <valentina2212.gonzalez@gmail.com>
2024-06-11 12:34:49 -0600

    added end game stats

---
+++ VideogameProject/Unity_FA/Assets/Scripts/Scripts that affect more that one sene/GameResults.cs
@@ -0,0 +1,9 @@
+public class GameResults{
+    public int Player_ID;
+    public int Resultado;
+
+    public GameResults(int id, int result){
+        Player_ID = id;
+        Resultado = result;
+    }
+}
